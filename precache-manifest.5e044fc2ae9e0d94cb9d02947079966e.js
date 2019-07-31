62c21c2246680d8f13a19e3fcd33799ae19e5a93
shivs077 <shivambisht.1998@gmail.com>
2019-07-31 15:46:44 +0530

    Updates

---
+++ precache-manifest.5e044fc2ae9e0d94cb9d02947079966e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdfbe9d78216bfd31e5b8d96414bb88",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "1636b91517dedd58d7b9",
+    "url": "/BurgerBuilder/static/css/main.854a9cff.chunk.css"
+  },
+  {
+    "revision": "6e05d8b4330b0c64bde4",
+    "url": "/BurgerBuilder/static/js/2.c2c6ca84.chunk.js"
+  },
+  {
+    "revision": "1636b91517dedd58d7b9",
+    "url": "/BurgerBuilder/static/js/main.4ac2de52.chunk.js"
+  },
+  {
+    "revision": "c4803fff7a346a4d9e6f",
+    "url": "/BurgerBuilder/static/js/runtime~main.70232762.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
