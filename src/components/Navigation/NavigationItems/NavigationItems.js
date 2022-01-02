bb1cbe58d0354cf372ef5fd3e5187720b0d5c92d
Shivam <shivam.bisht@thoughts2binary.com>
2022-01-02 18:22:31 +0530

    ui fixes

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 const NavigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/BurgerBuilder">Burger Builder</NavigationItem>
+    <NavigationItem link="/BurgerBuilder/">Burger Builder</NavigationItem>
     <NavigationItem link="/BurgerBuilder/orders">Orders</NavigationItem>
   </ul>
 );
